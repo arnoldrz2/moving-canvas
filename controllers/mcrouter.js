bfda553f1f8ec8a09642bff7ebe295cd66f068b5
arnold <arnoldrz2@gmail.com>
2018-03-11 17:35:34 -0500

    created route place holders

--- controllers/mcrouter.js
+++ controllers/mcrouter.js
@@ -6,17 +6,22 @@ var user = require("../models/user.js");
 
 // Create the routes
 router.get("/", function(req, res) {
-    res.render("index");
+    res.render("index"); //splash page
 });
-router.get("/strains", function(req, res) {
-    res.render("strains");
+router.get("/search", function(req, res) {
+    res.render("search"); //search page
 });
-router.get("/topten", function(req, res) {
-    res.render("topten");
+router.get("/artistaccountsetup", function(req, res) {
+    res.render("artaccset"); //artist account setup page
 });
-router.get("/account", function(req, res) {
-    res.render("account");
+router.get("/artistprofile", function(req, res) {
+    res.render("artprof"); //artist profile page
 });
-
+router.get("/useraccountsetup", function(req, res) {
+    res.render("useraccset"); //user account sestup page
+});
+router.get("/userprofile", function(req, res) {
+    res.render("userprof"); //user profile page
+});
 
 module.exports = router;
